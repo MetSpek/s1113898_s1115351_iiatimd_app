df7f59ee57dd95c617f3c46e4ce4ba7ab74471fe
Anoea <s1115351@student.hsleiden.nl>
2023-06-26 16:29:28 +0200

    ListEntry functionalities

--- Components/Home/home.gd
+++ Components/Home/home.gd
@@ -1,12 +1,21 @@
 extends Control
 
 @onready var imageEntry = preload("res://Components/Home/imageEntry.tscn")
+@onready var listEntry = preload("res://Components/Home/listEntry.tscn")
 @onready var card_container = $ContentContainer/ScrollContainer/CardContainer
+
+var entryVersion
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if GlobalHandler.current_diary_view == 'normal':
+		entryVersion = imageEntry
+		
+	else:
+		entryVersion = listEntry
+		
 	for entry in GlobalHandler.diary_entries:
-		var card = imageEntry.instantiate()
+		var card = entryVersion.instantiate()
 		card.title = entry.title
 		card.date = entry.date
 		
